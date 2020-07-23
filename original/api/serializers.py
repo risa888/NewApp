3c8312f9080dfd8403711c263e7a9d9a2bda1b60
risa888 <rs.04090325@gmail.com>
2020-07-23 12:58:40 +0900

    likes_count作成in post serializer

--- original/api/serializers.py
+++ original/api/serializers.py
@@ -26,7 +26,7 @@ class CommentsSerializer(serializers.ModelSerializer):
 
 
 class PostSerializer(serializers.ModelSerializer):
-    # comments_count = serializers.SerializerMethodField(read_only=True)
+    follow_count = serializers.SerializerMethodField(read_only=True)
     likes_count = serializers.SerializerMethodField(read_only=True)
     author = serializers.StringRelatedField(read_only=True)
     slug = serializers.SlugField(read_only=True)
@@ -34,10 +34,11 @@ class PostSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Post
-        fields = '__all__'
+        exclude = ['likes','follow']
 
-    # def get_comments_count(self, instance):
-    #     return instance.comments.count()
+
+    def get_follow_count(self, instance):
+        return instance.follow.count()
 
     def get_likes_count(self, instance):
         return instance.likes.count()
