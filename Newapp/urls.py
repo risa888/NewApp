b99b89b9d94f25ff8c61773a3e02be0b5e210cea
risa888 <rs.04090325@gmail.com>
2020-07-29 23:09:20 +0900

    画像表示 OK

--- Newapp/urls.py
+++ Newapp/urls.py
@@ -48,9 +48,8 @@ urlpatterns = [
     path('api/rest-auth/registration/',
          include('rest_auth.registration.urls')),
 
-     re_path(r"^.*$", IndexTemplateView.as_view(), name="entry-point"),
 
-    #  re_path(r"^(?!.*/media).+$", IndexTemplateView.as_view(), name="entry-point")
+     re_path(r"^(?!.*/media).+$", IndexTemplateView.as_view(), name="entry-point")
 
 ]
 
