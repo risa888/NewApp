285502dfaaa0c2dc70296a9e7ef39ce21c9c5f43
risa888 <rs.04090325@gmail.com>
2020-08-06 20:01:21 +0900

    フロントに表示完了

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,32 +1,82 @@
 <template>
   <div class="home">
-    <div class="container">
-    <p>Flowers Home</p>
+    <div class="container mt-2">
+      <div v-for="post in posts"
+           :key="post.pk">
+        <p class="mb-0">Posted by:
+          <span class="post-author">{{ post.author }}</span>
+        </p>
+        <h2>
+          <router-link
+            :to="{ name: 'post', params: { slug: post.slug } }"
+            class="question-link"
+            >{{ post.caption }}
+          </router-link>
+        </h2>
+        
+      <div class="my-4">
+        <p v-show="loadingPosts">...loading...</p>
+        <button
+          v-show="next"
+          @click="getPosts"
+          class="btn btn-sm btn-outline-success"
+          >Load More
+        </button>
+      </div>
     </div>
+  </div>
   </div>
 </template>
 
 <script>
-import { apiService } from "../common/api.service.js";
+import { apiService } from "@/common/api.service.js";
 export default {
-  name: 'home',
+  name: "home",
   data() {
     return {
-      post: []
+      posts: [],
+      next: null,
+      loadingPosts: false
     }
   },
   methods: {
-    getPost() {
+    getPosts() {
+      // make a GET Request to the questions list endpoint and populate the questions array
       let endpoint = "/api/post/";
+      if (this.next) {
+        endpoint = this.next;
+      }
+      this.loadingPosts = true;
       apiService(endpoint)
         .then(data => {
-          this.post.push(...data.results)
+          this.posts.push(...data)
+          this.loadingPosts = false;
+          if (data.next) {
+            this.next = data.next;
+          } else {
+            this.next = null;
+          }
         })
     }
   },
   created() {
-    this.getPost()
-    console.log(this.post)
+    this.getPosts()
+    document.title = "QuestionTime";
   }
 };
 </script>
+
+<style scoped>
+.question-author {
+  font-weight: bold;
+  color: #DC3545;
+}
+.question-link {
+  font-weight: bold;
+  color: black;
+}
+.question-link:hover {
+  color: #343A40;
+  text-decoration: none;
+}
+</style>
